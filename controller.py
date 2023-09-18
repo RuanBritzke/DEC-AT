b598feab9c6b091425da7101a596b6d3a5b7ca95
Ruan Britzke <ruan.w.brtizke@outlook.com>
2023-09-18 19:45:21 -0300

    Custom pandastable toolbar

--- controller.py
+++ controller.py
@@ -3,36 +3,35 @@ import os
 from model import *
 from view import View
 
-ALL = 'All'
-SE = 'SE'
-BUS = 'BUS'
+ALL = "All"
+SE = "SE"
+BUS = "BUS"
 
-class Controller():
-    
-    table_collection = dict()
-    view_table_collection = dict()
+
+class Controller:
+    view_tab_collection = dict()
     view = None
 
-
-    def __init__(self, model : Model, view : View):
+    def __init__(self, model: Model, view: View):
         self.model = model
         self.view = view
 
         self.view.set_controller(self)
-    
 
     def import_file(self):
-            file = self.view.askfilewindow()
-            file_ext = os.path.splitext(file)[-1]
-            if file_ext != '.PWF':
-                self.view.status_bar.set(f'FALHA! Extensão não aceita: "{file_ext}"')
-            else:
-                self.view.status_bar.set(f'Importando aquivo de: "{file}"')
-                buses, lines = self.model.create_net(file)
-                self.view.status_bar.set(f'Rede importada: {buses} Barras e {lines} conexões!')
-                self.view.dec.enable()
+        file = self.view.askfilewindow()
+        file_ext = os.path.splitext(file)[-1]
+        if file_ext != ".PWF":
+            self.view.status_bar.set(f'FALHA! Extensão não aceita: "{file_ext}"')
+        else:
+            self.view.status_bar.set(f'Importando aquivo de: "{file}"')
+            buses, lines = self.model.create_net(file)
+            self.view.status_bar.set(
+                f"Rede importada: {buses} Barras e {lines} conexões!"
+            )
+            self.view.dec.enable()
 
-    def get_options_cbox_values(self, to : Literal['SUB', 'BUS']):
+    def get_options_cbox_values(self, to: Literal["SUB", "BUS"]):
         if to == SUB:
             return sorted(self.model.load_subs)
         elif to == BUS:
@@ -40,21 +39,27 @@ class Controller():
         else:
             return None
 
-    def processingData(self, indexes : list, data : list, sub, buses):
-        visited_failures = set()    
-        for bus in sorted(buses, key= lambda x: self.model.network.nodes[x][VOLTAGE_LEVEL], reverse=True):
+    def processingData(self, indexes: list, data: list, sub, buses):
+        visited_failures = set()
+        for bus in sorted(
+            buses,
+            key=lambda x: self.model.network.nodes[x][VOLTAGE_LEVEL],
+            reverse=True,
+        ):
             failureModes = self.model.failure_modes_table(bus)
             for index, failures, _ in failureModes.itertuples():
-                if failures in visited_failures: continue
+                if failures in visited_failures:
+                    continue
                 indexes.append((sub, bus, index))
                 data.append([list(failures)])
                 visited_failures.add(failures)
         return indexes, data
 
-    def generateFailureTable(self, scope: Literal['ALL', 'SE', 'SUB'], entry: str|int|None = None):
-        
+    def generateFailureTable(
+        self, scope: Literal["ALL", "SE", "SUB"], entry: str | int | None = None
+    ):
         indexes = list()
-        data  = list()
+        data = list()
         if scope == ALL:
             for kw, buses in self.model.load_subs_dict.items():
                 indexes, data = self.processingData(indexes, data, kw, buses)
@@ -64,24 +69,30 @@ class Controller():
             indexes, data = self.processingData(indexes, data, entry, buses)
 
         elif scope == BUS:
-            
             sub = self.model.network.nodes[entry][SUB]
             indexes, data = self.processingData(indexes, data, sub, [entry])
-        
-        mi = pd.MultiIndex.from_tuples(indexes, names= ['SE', 'BARRA', 'N'])
-        df = pd.DataFrame(data, index= mi, columns = ['FALHAS'])
-        maxFailureLen = df['FALHAS'].apply(len).max()
+
+        mi = pd.MultiIndex.from_tuples(indexes, names=["SE", "BARRA", "N"])
+        df = pd.DataFrame(data, index=mi, columns=["FALHAS"])
+        maxFailureLen = df["FALHAS"].apply(len).max()
 
         for i in range(maxFailureLen):
-            df[f'FALHA_{i+1}'] = df['FALHAS'].apply(lambda x: x[i] if len(x) > i else None)
-        df.drop(columns='FALHAS', inplace=True)
+            df[f"FALHA_{i+1}"] = df["FALHAS"].apply(
+                lambda x: x[i] if len(x) > i else None
+            )
+        df.drop(columns="FALHAS", inplace=True)
 
         flatDf = df.reset_index()
         return flatDf
 
-    def computeUnavailabilty(self, entry: str|None = None):
+    def computeUnavailabilty(self, entry: str | None = None):
+        if entry in self.view_tab_collection.keys():
+            old_tab_name = self.view_tab_collection[entry][0]
+            old_tab = self.view.output.tab_collection[old_tab_name]
+            self.view.output.notebook.select(old_tab)
+            return
         if entry is None:
-            entry = 'Todas SEs'
+            entry = "Todas SEs"
             df = self.generateFailureTable(scope=ALL)
         elif entry.isalpha():
             df = self.generateFailureTable(scope=SE, entry=entry)
@@ -91,21 +102,28 @@ class Controller():
         for row in df.itertuples():
             index, se, bus, n = row[0:4]
             failures = row[4:]
-            df.at[index, 'ORDEM'] = int(len([failure for failure in failures if failure is not None])) # TODO: Not working as intended!
-            df.at[index, 'IND'] = self.model.unavailabilty(failures)
-            df.at[index, 'DEC'] = None
-    
-        self.table_collection[entry] = df
-        self.view_table_collection[entry] = self.view_table(df)
+            df.at[index, "ORDEM"] = int(
+                len([failure for failure in failures if failure is not None])
+            )
+            df.at[index, "IND"] = self.model.unavailabilty(failures)
+            df.at[index, "DEC"] = None
 
-        self.view.output.add_table(title= f'{entry}', view_table = self.view_table_collection[entry])
-        
+        view_table = self.view_table(df)
 
-    def view_table(self, df : pd.DataFrame):
+        self.view_tab_collection[entry] = (entry, df, view_table)
+
+        self.view.output.add_table(title=f"{entry}", view_table=view_table)
+
+    def view_table(self, df: pd.DataFrame):
         view_table = df.copy()
-        view_table['N'] = view_table['N'] + 1
-        
-        for col in df.columns[3:-3]: # iterate over all failure columns
-            view_table[col] = view_table[col].map(self.model.get_edge_name, na_action= 'ignore')
-            
+        view_table["N"] = view_table["N"] + 1
+
+        for col in df.columns[3:-3]:  # iterate over all failure columns
+            view_table[col] = view_table[col].map(
+                self.model.get_edge_name, na_action="ignore"
+            )
+
         return view_table
+
+    def del_tab(self, entry):
+        del self.view_tab_collection[entry]
--- view.py
+++ view.py
@@ -1,70 +1,61 @@
 import tkinter as tk
 import tkinter.filedialog
-from tkinter import ttk
 import pandas as pd
-import pandastable as pdt
+from tkinter import ttk
 
-from typing import Literal
-
+from custompandastable import *
 
 pd.options.mode.chained_assignment = None  # default='warn'
-
-class MyTable(pdt.Table):
-
-    def __init__(self, **kwargs):
-        pass
 
 
 class StatusBar(tk.Frame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.config(highlightbackground='black', highlightthickness=1)
-        self.label = tk.Label(self, text= '', bg = 'white')
-        self.label.pack(side='left')
-        self.pack(side='bottom', fill='x')
+        self.config(highlightbackground="black", highlightthickness=1)
+        self.label = tk.Label(self, text="", bg="white")
+        self.label.pack(side="left")
+        self.pack(side="bottom", fill="x")
 
     def set(self, newText):
         self.label.config(text=newText)
 
     def clear(self):
-        self.label.config(text='')
+        self.label.config(text="")
+
 
 class DEC(tk.Frame):
-    
     cbox_values = None
 
     def __init__(self, root, master, controller, **kwargs):
         self.root = root
         self.master = master
         self.controller = controller
-        super().__init__(master,  **kwargs)
-        tk.Label(self, text='Calcular DEC:').pack(anchor='nw')
+        super().__init__(master, **kwargs)
+        tk.Label(self, text="Calcular DEC:").pack(anchor="nw")
         self.create_function_selection_buttons()
         self.search_box()
-        self.pack(anchor='nw')
-        ttk.Separator(master).pack(pady=5, fill='x', anchor='n')
+        self.pack(anchor="nw")
+        ttk.Separator(master).pack(pady=5, fill="x", anchor="n")
 
     def startSearch(self, *args):
-        self.root.status_bar.set(f'Calculando Indisponibilidade para {self.cbox.get()}')
-        self.controller.computeUnavailabilty(entry = self.cbox.get())
-        self.root.status_bar.set('Pronto!')
+        self.root.status_bar.set(f"Calculando Indisponibilidade para {self.cbox.get()}")
+        self.controller.computeUnavailabilty(entry=self.cbox.get())
+        self.root.status_bar.set("Pronto!")
 
     def search_box(self):
         root = tk.Frame(self)
         self.vars = tk.StringVar()
         self.cbox = ttk.Combobox(
             root,
-            state='disabled',
+            state="disabled",
             textvariable=self.vars,
-            postcommand= self.update_cblist)
+            postcommand=self.update_cblist,
+        )
         self.cbox.grid(row=0, column=0, padx=5, pady=5)
-        self.search = tk.Button(
-            root, 
-            text="Procurar", 
-            state='disabled')
+        self.search = tk.Button(root, text="Procurar", state="disabled")
         self.search.grid(row=0, column=1, padx=5, pady=5)
-        root.pack(fill='x', expand=True)
-        self.search.bind('<ButtonRelease-1>', self.startSearch)
+        root.pack(fill="x", expand=True)
+        self.search.bind("<ButtonRelease-1>", self.startSearch)
 
     def create_function_selection_buttons(self):
         labels = ["Todas SEs", "Por SE", "Por BARRA"]
@@ -74,184 +65,193 @@ class DEC(tk.Frame):
             radio_btn = tk.Radiobutton(
                 self,
                 text=label,
-                state= 'disabled',
-                variable= self.var,
-                value = i,
-                command= lambda: self.radio_button_selection(self.var.get()))
-            radio_btn.pack(anchor='nw')
+                state="disabled",
+                variable=self.var,
+                value=i,
+                command=lambda: self.radio_button_selection(self.var.get()),
+            )
+            radio_btn.pack(anchor="nw")
 
     def radio_button_selection(self, option: int):
         if option == 0:
-            self.cbox.config(state= 'disabled')
-            self.cbox_values = 'Todas SEs'
-            self.cbox.set('Todas SEs')
+            self.cbox.config(state="disabled")
+            self.cbox_values = "Todas SEs"
+            self.cbox.set("Todas SEs")
         if option == 1:
-            self.cbox.config(state= 'normal')
-            self.cbox_values = self.controller.get_options_cbox_values(to = 'SUB')
+            self.cbox.config(state="normal")
+            self.cbox_values = self.controller.get_options_cbox_values(to="SUB")
             self.cbox.set(self.cbox_values[0])
         elif option == 2:
-            self.cbox.config(state= 'normal')
-            self.cbox_values = self.controller.get_options_cbox_values(to = 'BUS')
+            self.cbox.config(state="normal")
+            self.cbox_values = self.controller.get_options_cbox_values(to="BUS")
             self.cbox.set(self.cbox_values[0])
 
-    def enable(self, _state='normal'):
-        def set_state(widget:tk.Widget):
+    def enable(self, _state="normal"):
+        def set_state(widget: tk.Widget):
             for ch in widget.winfo_children():
                 set_state(ch)
-            if 'state' in  widget.keys():
-                widget.config(state = _state)
+            if "state" in widget.keys():
+                widget.config(state=_state)
+
         set_state(self)
 
     def disable(self):
-        self.enable('disable')
+        self.enable("disable")
 
     def update_cblist(self):
-        self.cbox['values'] = self.cbox_values
+        self.cbox["values"] = self.cbox_values
+
 
 class ParameterInputFrame(tk.Frame):
-
-    def __init__(self, master, view_table : pd.DataFrame, **kwargs):
+    def __init__(self, master, view_table: pd.DataFrame, **kwargs):
         super().__init__(master, **kwargs)
-        self.columnconfigure(0, weight= 1)
-        self.columnconfigure(1, weight= 1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
         self.view_table = view_table
-        if view_table.columns[4] == 'FALHA_2':
-            self.failures = self.view_table.apply(lambda row: f"{row['FALHA_1']} <-> {row['FALHA_2']}" if row['FALHA_2'] is not None else f"{row['FALHA_1']}", axis = 1).tolist()
-        else: self.failures = self.view_table['FALHA_1'].tolist()
+        if view_table.columns[4] == "FALHA_2":
+            self.failures = self.view_table.apply(
+                lambda row: f"{row['FALHA_1']} <-> {row['FALHA_2']}"
+                if row["FALHA_2"] is not None
+                else f"{row['FALHA_1']}",
+                axis=1,
+            ).tolist()
+        else:
+            self.failures = self.view_table["FALHA_1"].tolist()
         self.create_widgets()
-        
 
     def create_widgets(self):
         self.create_failure_selection_frame()
         self.create_failure_type_frame()
 
     def create_failure_selection_frame(self):
-        failure_selection_frame =  tk.Frame(self)
-        failure_selection_label = tk.Label(failure_selection_frame, text= "Selecione a Falha:", justify= 'left')
+        failure_selection_frame = tk.Frame(self)
+        failure_selection_label = tk.Label(
+            failure_selection_frame, text="Selecione a Falha:", justify="left"
+        )
 
         self.failure_vars = tk.StringVar()
-        
+
         self.failure_selection_cbox = ttk.Combobox(
             failure_selection_frame,
-            values= self.failures,
-            textvariable= self.failure_vars)
-        
+            values=self.failures,
+            textvariable=self.failure_vars,
+        )
+
         self.failure_selection_cbox.set(self.failures[0])
-        
-        failure_selection_label.pack(anchor='nw', padx=5, pady=5, expand= True)
-        self.failure_selection_cbox.pack(fill= 'x', padx=5, pady=5, expand=True)
-        
-        failure_selection_frame.grid(row= 0, column= 0, padx= 5, sticky='ew')
+
+        failure_selection_label.pack(anchor="nw", padx=5, pady=5, expand=True)
+        self.failure_selection_cbox.pack(fill="x", padx=5, pady=5, expand=True)
+
+        failure_selection_frame.grid(row=0, column=0, padx=5, sticky="ew")
 
     def create_failure_type_frame(self):
         failure_type_frame = tk.Frame(self)
 
         failure_type_label = tk.Label(
-            failure_type_frame, 
-            text= 'Selecione o Tipo de Falha', 
-            justify='left')
-        
+            failure_type_frame, text="Selecione o Tipo de Falha", justify="left"
+        )
+
         self.type_vars = tk.StringVar()
 
         self.failure_type_cbox = ttk.Combobox(
             failure_type_frame,
-            values = ['Tipo 1', 'Tipo 2', 'Tipo 3'],
-            textvariable= self.type_vars
+            values=["Tipo 1", "Tipo 2", "Tipo 3"],
+            textvariable=self.type_vars,
         )
-        self.failure_type_cbox.set('Tipo 1')
+        self.failure_type_cbox.set("Tipo 1")
 
-        failure_type_label.pack(anchor='nw', padx=5, pady=5, expand= True)
-        self.failure_type_cbox.pack(fill= 'both', expand= True)
+        failure_type_label.pack(anchor="nw", padx=5, pady=5, expand=True)
+        self.failure_type_cbox.pack(fill="both", expand=True)
 
-        failure_type_frame.grid(row= 0, column= 1, padx= 5, sticky='ew')
+        failure_type_frame.grid(row=0, column=1, padx=5, sticky="ew")
 
     def forget(self) -> None:
         for widget in self.winfo_children():
             widget.grid_forget()
 
-class OutputNotebook:
 
-    def __init__(self, master : 'View', controller, **kwargs):
+class OutputNotebook:
+    tab_collection = dict()
+
+    def __init__(self, master: "View", controller, **kwargs):
         self.master = master
         self.controller = controller
         self.notebook = CustomNotebook(master)
-        self.notebook.pack(pady=(8,0), anchor='ne', fill='both', expand=True)
-        self.add_table('Exemplo')
+        self.notebook.pack(pady=(8, 0), anchor="ne", fill="both", expand=True)
+        self.add_table("Exemplo")
+        cmd = self.notebook.register(self.on_tab_closed)
+        self.notebook.tk.call(
+            "bind", self.notebook, "<<NotebookTabClosed>>", cmd + " %d"
+        )
 
-    def add_table(
-            self, 
-            title : str, 
-            view_table: pd.DataFrame | None = None):
-        """ ## Parameters:
-        title: str
-            Titulo da nova janela a ser apresentada
+    def add_table(self, title: str, view_table: pd.DataFrame | None = None):
+        tab = tk.Frame(self.notebook)
 
-        table: pd.DataFrame | None
-            Tabela a ser apresentada pelo programa
+        self.tab_collection[title] = tab
 
-        df: pd.Dataframe | None
-            Tabela original de dados. (Será testado se ainda é necessário)
-        """
-        ntab = tk.Frame(self.notebook)
-        tableWindow = tk.Frame(ntab)
-        
+        tableWindow = tk.Frame(tab)
+
         if view_table is None:
-            ntabLabel = tk.Label(ntab,
-                                text="Seus outputs serão gerados aqui!",
-                                justify='left')
-            ntabLabel.pack(anchor='n', fill='x', expand=True)
+            ntabLabel = tk.Label(
+                tab, text="Seus outputs serão gerados aqui!", justify="left"
+            )
+            ntabLabel.pack(anchor="n", fill="x", expand=True)
         elif view_table.empty:
             return
         else:
-            model = pdt.TableModel(view_table)
-            self.table = pdt.Table(
-                tableWindow, 
-                model=model, 
-                showtoolbar=True, 
-                editable=False)
+            model = TableModel(view_table)
+            self.table = MyCustonPandasTable(
+                tableWindow, model=model, showtoolbar=True, editable=False
+            )
             self.table.show()
 
-            tableWindow.pack(anchor='n', fill='x')
-            ttk.Separator(ntab).pack(pady=5, fill='x', anchor='n')
+            tableWindow.pack(anchor="n", fill="x")
+            ttk.Separator(tab).pack(pady=5, fill="x", anchor="n")
 
-            inputsw = ParameterInputFrame(ntab, view_table)
-            inputsw.pack(anchor='n', fill='both', expand=True)
+            inputsw = ParameterInputFrame(tab, view_table)
+            inputsw.pack(anchor="n", fill="both")
 
-        self.notebook.add(ntab, text=title)
-        self.notebook.select(ntab)
+        self.notebook.add(tab, text=title)
+        self.notebook.select(tab)
         return
 
-class View:
+    def on_tab_closed(self, data):
+        del self.tab_collection[data]
+        if data != "Exemplo":
+            self.controller.del_tab(data)
 
-    def __init__(self, master : tk.Tk):
+
+class View:
+    def __init__(self, master: tk.Tk):
         self.master = master
-        self.status_bar = StatusBar(self.master, bg='white')
-        self.options = tk.LabelFrame(self.master, text = 'Opções:')
-        self.options.pack(side='left', fill='y', anchor='w')
-        self.status_bar.set('Importe uma rede!')
+        self.status_bar = StatusBar(self.master, bg="white")
+        self.options = tk.LabelFrame(self.master, text="Opções:")
+        self.options.pack(side="left", fill="y", anchor="w")
+        self.status_bar.set("Importe uma rede!")
 
     def set_controller(self, controller):
         self.controller = controller
         self.my__post__init__()
 
     def my__post__init__(self):
-        self.create_menu() 
+        self.create_menu()
         self.dec = DEC(self, self.options, self.controller)
         self.output = OutputNotebook(self.master, self.controller)
 
     def create_menu(self):
         menubar = tk.Menu(self.master, tearoff=False)
-        self.master.config(menu = menubar)
+        self.master.config(menu=menubar)
         menubar.add_command(
-            label= 'Importar',
-            command= self.controller.import_file,
+            label="Importar",
+            command=self.controller.import_file,
         )
 
     def askfilewindow(self):
         return tkinter.filedialog.askopenfilename(
-            defaultextension= ".PWF", 
-            filetypes=[('All files', '*'), ('Arquivo Texto do Anarede', '.PWF')]
+            defaultextension=".PWF",
+            filetypes=[("All files", "*"), ("Arquivo Texto do Anarede", ".PWF")],
         )
+
 
 class CustomNotebook(ttk.Notebook):
     """A ttk Notebook with close buttons on each tab"""
@@ -278,16 +278,16 @@ class CustomNotebook(ttk.Notebook):
 
         if "close" in element:
             index = self.index("@%d,%d" % (event.x, event.y))
-            self.state(['pressed'])
+            self.state(["pressed"])
             self._active = index
             return "break"
 
     def on_close_release(self, event):
         """Called when the button is released"""
-        if not self.instate(['pressed']):
+        if not self.instate(["pressed"]):
             return
 
-        element =  self.identify(event.x, event.y)
+        element = self.identify(event.x, event.y)
         if "close" not in element:
             # user moved the mouse off of the close button
             return
@@ -295,8 +295,9 @@ class CustomNotebook(ttk.Notebook):
         index = self.index("@%d,%d" % (event.x, event.y))
 
         if self._active == index:
+            closed_tab_text = self.tab(index, "text")
             self.forget(index)
-            self.event_generate("<<NotebookTabClosed>>")
+            self.event_generate("<<NotebookTabClosed>>", data=closed_tab_text)
 
         self.state(["!pressed"])
         self._active = None
@@ -304,44 +305,77 @@ class CustomNotebook(ttk.Notebook):
     def __initialize_custom_style(self):
         style = ttk.Style()
         self.images = (
-            tk.PhotoImage("img_close", data='''
+            tk.PhotoImage(
+                "img_close",
+                data="""
                 R0lGODlhCAAIAMIBAAAAADs7O4+Pj9nZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
                 d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
                 5kEJADs=
-                '''),
-            tk.PhotoImage("img_closeactive", data='''
+                """,
+            ),
+            tk.PhotoImage(
+                "img_closeactive",
+                data="""
                 R0lGODlhCAAIAMIEAAAAAP/SAP/bNNnZ2cbGxsbGxsbGxsbGxiH5BAEKAAQALAAA
                 AAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU5kEJADs=
-                '''),
-            tk.PhotoImage("img_closepressed", data='''
+                """,
+            ),
+            tk.PhotoImage(
+                "img_closepressed",
+                data="""
                 R0lGODlhCAAIAMIEAAAAAOUqKv9mZtnZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
                 d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
                 5kEJADs=
-            ''')
+            """,
+            ),
         )
 
-        style.element_create("close", "image", "img_close",
-                            ("active", "pressed", "!disabled", "img_closepressed"),
-                            ("active", "!disabled", "img_closeactive"), border=8, sticky='')
+        style.element_create(
+            "close",
+            "image",
+            "img_close",
+            ("active", "pressed", "!disabled", "img_closepressed"),
+            ("active", "!disabled", "img_closeactive"),
+            border=8,
+            sticky="",
+        )
         style.layout("CustomNotebook", [("CustomNotebook.client", {"sticky": "nswe"})])
-        style.layout("CustomNotebook.Tab", [
-            ("CustomNotebook.tab", {
-                "sticky": "nswe",
-                "children": [
-                    ("CustomNotebook.padding", {
-                        "side": "top",
+        style.layout(
+            "CustomNotebook.Tab",
+            [
+                (
+                    "CustomNotebook.tab",
+                    {
                         "sticky": "nswe",
                         "children": [
-                            ("CustomNotebook.focus", {
-                                "side": "top",
-                                "sticky": "nswe",
-                                "children": [
-                                    ("CustomNotebook.label", {"side": "left", "sticky": ''}),
-                                    ("CustomNotebook.close", {"side": "left", "sticky": ''}),
-                                ]
-                        })
-                    ]
-                })
-            ]
-        })
-    ])
+                            (
+                                "CustomNotebook.padding",
+                                {
+                                    "side": "top",
+                                    "sticky": "nswe",
+                                    "children": [
+                                        (
+                                            "CustomNotebook.focus",
+                                            {
+                                                "side": "top",
+                                                "sticky": "nswe",
+                                                "children": [
+                                                    (
+                                                        "CustomNotebook.label",
+                                                        {"side": "left", "sticky": ""},
+                                                    ),
+                                                    (
+                                                        "CustomNotebook.close",
+                                                        {"side": "left", "sticky": ""},
+                                                    ),
+                                                ],
+                                            },
+                                        )
+                                    ],
+                                },
+                            )
+                        ],
+                    },
+                )
+            ],
+        )
